0a71ccf5e070097592c3ca61cadb2be8db160edb
Sophiexin9636 <sophiexin036@gmail.com>
2020-07-14 20:59:50 +0800

    add cnn

--- ML/cnn.py
+++ ML/cnn.py
@@ -1,54 +1,165 @@
-from preprocess import *
-import keras
-from keras.models import Sequential
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+import keras.losses  
+from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+from PIL import Image
+import os, sys, getopt
 
-# loading data
-X_train, X_test, y_train, y_test = get_train_test()
-X_train = X_train.reshape(X_train.shape[0], 20, 11, 1)
-X_test = X_test.reshape(X_test.shape[0], 20, 11, 1)
+dict_labels = {"incorrect":0, "correct":1}
 
-# one-hot encoding
-y_train_hot = to_categorical(y_train)
-y_test_hot = to_categorical(y_test)
-print("X_train.shape=", X_train.shape)
+input_img_path = "/content/drive/My Drive/Colab Notebooks/soundAnalysis/img/"
+correct_img_path = "/content/drive/My Drive/Colab Notebooks/soundAnalysis/img/correct/"
+correct_np_path = ""
+incorrect_img_path = "/content/drive/My Drive/Colab Notebooks/soundAnalysis/img/incorrect/"
+incorrect_np_path = ""
 
-# create Sequential model
-model = Sequential()
-# convolution layer，filter (output size) = 32
-model.add(Conv2D(32, kernel_size=(2, 2), activation='relu', input_shape=(20, 11, 1)))
-# Pooling layer. and take maximum
-model.add(MaxPooling2D(pool_size=(2, 2)))
-# Dropout層隨機斷開輸入神經元，用於防止過度擬合，斷開比例:0.25
-model.add(Dropout(0.25))
-# Flatten層把多維的輸入一維化，常用在從卷積層到全連接層的過渡。
-model.add(Flatten())
-# FNN layer: 128 output
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.25))
-# Add output layer
-model.add(Dense(3, activation='softmax'))
-# Compile: choose loss function、optimize method
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
-              metrics=['accuracy'])
+def save_data_npy(imgPath, npPath, file_arr):
+    # Load Image, and save to npy
+    for i, filename in enumerate(file_arr):
+        file = imgPath + filename
+        if os.path.isdir(file):
+            continue
+        img_data = np.array(Image.open(file).convert('RGB')) # Image.open(cur_file).convert('L')
+        img_data = np.expand_dims(img_data, axis=0)
+        np.save(npPath + str(i), img_data)
 
-# training, 訓練過程會存在 train_history
-model.fit(X_train, y_train_hot, batch_size=100, epochs=200, verbose=1, validation_data=(X_test, y_test_hot))
+def show_train_history(train_history, train, validation):  
+    plt.plot(train_history.history[train])  
+    plt.plot(train_history.history[validation]) # validation : test
+    plt.title('Train History')  
+    plt.ylabel('Accuracy')  
+    plt.xlabel('Epoch')  
+    plt.legend(['train', 'validation'], loc='upper left')  
+    plt.show() 
 
+def cnn():
+    # Get available labels
+    correct_labels = os.listdir(correct_img_path)
+    save_data_npy(correct_img_path, correct_np_path, correct_labels)
+    correct_labels = os.listdir(correct_np_path)
 
-X_train = X_train.reshape(X_train.shape[0], 20, 11, 1)
-X_test = X_test.reshape(X_test.shape[0], 20, 11, 1)
-score = model.evaluate(X_test, y_test_hot, verbose=1)
+    # Getting first arrays
+    X = np.load(correct_np_path + correct_labels[0])
+    cnn_shape = X.shape[1:]
+    y = np.zeros(X.shape[0])
+    y[0] = dict_labels["correct"]
 
-# 模型存檔
-from keras.models import load_model
-model.save('ASR.h5')  # creates a HDF5 file 'model.h5'
+    # Append all of the correct dataset into one single array, same goes for y
+    for i, label in enumerate(correct_labels[1:]):
+        x = np.load(correct_np_path + label)
+        X = np.vstack((X, x))
+        y = np.append(y, np.full(x.shape[0], fill_value=dict_labels["correct"]))
+    assert X.shape[0] == len(y)
 
+    incorrect_labels = os.listdir(incorrect_img_path)
+    save_data_npy(incorrect_img_path, incorrect_np_path, incorrect_labels)
+    incorrect_labels = os.listdir(incorrect_np_path)
+    # Append all of the incorrect dataset into one single array, same goes for y
+    for i, label in enumerate(incorrect_labels):
+        x = np.load(incorrect_np_path + label)
+        X = np.vstack((X, x))
+        y = np.append(y, np.full(x.shape[0], fill_value=dict_labels["incorrect"]))
+    print(X.shape[0], len(y))
+    assert X.shape[0] == len(y)
 
-# 預測(prediction)
-mfcc = wav2mfcc('./data/happy/012c8314_nohash_0.wav')
-mfcc_reshaped = mfcc.reshape(1, 20, 11, 1)
-print("labels=", get_labels())
-print("predict=", np.argmax(model.predict(mfcc_reshaped)))
+    # loading data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= (1 - 0.6), random_state=48, shuffle=True)
+    #X_train = X_train.reshape(X_train.shape[0], x.shape[0], x.shape[1], x.shape[2])
+    #X_test = X_test.reshape(X_test.shape[0], x.shape[0], x.shape[1], x.shape[2])
+
+    # feature normalize
+    X_train_normalize = X_train / 255
+    X_test_normalize = X_test / 255
+
+    # one-hot encoding
+    y_train_hot = to_categorical(y_train)
+    y_test_hot  = to_categorical(y_test)
+
+    # create Sequential model
+    model = Sequential()
+    model.add(Conv2D(filters=16, kernel_size=(5, 5), padding='same', input_shape = cnn_shape, activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(filters=36, kernel_size=(5, 5), padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    # Flatten
+    model.add(Flatten())
+
+    # FNN layer: 128 output
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+
+    # Add output layer
+    model.add(Dense(2, activation='softmax'))
+
+    print(model.summary())
+
+    # Compile: choose loss function、optimize method
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
+    # training
+    train_history = model.fit(X_train_normalize, y_train_hot, 
+                              batch_size=10, 
+                              epochs=100, 
+                              verbose=1, 
+                              validation_data=(X_test_normalize, y_test_hot))
+
+    # save model
+    model.save('ASR.h5')  # creates a HDF5 file 'model.h5'
+
+    # Display loss function, training result
+    score = model.evaluate(X_test_normalize, y_test_hot, verbose=1)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+
+    # show training history
+    show_train_history(train_history, 'accuracy', 'val_accuracy')
+    show_train_history(train_history, 'loss', 'val_loss')
+
+def main(argv):
+    global input_img_path
+    global correct_img_path, correct_np_path
+    global incorrect_img_path, incorrect_np_path
+
+    try:
+        opts, args = getopt.getopt(argv, "dhi:", ["inpath="])
+    except getopt.GetoptError:
+        print('usage: python3 cnn.py -i <inputImagePath>')
+        sys.exit(2)
+    else:
+        for opt, arg in opts:
+            if opt == '-h':
+                print('usage: python3 cnn.py -i <inputImagePath>')
+                sys.exit()
+            elif opt == '-d':
+                break
+            elif opt in ("-i", "--inpath"):
+                if arg[-1] != '/':
+                    input_img_path = arg + '/'
+                else:
+                    input_img_path = arg
+        correct_img_path = input_img_path + "correct/"
+        correct_np_path = correct_img_path + "np/"
+        incorrect_img_path = input_img_path + "incorrect/"
+        incorrect_np_path = incorrect_img_path + "np/"
+        os.makedirs(correct_np_path, exist_ok=True)
+        os.makedirs(incorrect_np_path, exist_ok=True)
+        print('Input Correct Data Path： ', correct_img_path)
+        print('Numpy Data Save Path: ', correct_np_path)
+        print('Input Incorrect Data Path： ', incorrect_img_path)
+        print('Numpy Data Save Path: ', incorrect_np_path)
+        cnn()
+        print("\nCNN Training Has Done!\n")
+
+if( __name__ == '__main__'):
+    if (sys.argv.__len__()) > 1:
+        main(sys.argv[1:])
+    else:
+        print('usage: python3 cnn.py -i <inputImagePath>')
